9a4a74e057814811771791653ac7d9e7cdc19146
chandan <chandankumar018@gmail.com>
2022-02-11 15:13:54 +0530

    updated name length condition

--- customer.py
+++ customer.py
@@ -1,2 +1,10 @@
 customer = input("What is your Name ? ")
 print(f"Hi {customer}  Nice to meet you")
+
+if len(customer) <3:
+    print("Name is short")
+elif len(customer)> 10:
+    print("name is too long")
+else:
+    print("Name looks good")
+    
